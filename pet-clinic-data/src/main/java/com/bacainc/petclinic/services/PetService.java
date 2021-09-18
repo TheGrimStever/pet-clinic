8d4b2e460065a8cfbca5063bc568ff77f8fa71c5
SBaca <senior.baca@gmail.com>
2021-09-17 20:20:30 -0600

    Created Interfaces for services.  Closes #5

---
+++ pet-clinic-data/src/main/java/com/bacainc/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package com.bacainc.petclinic.services;
+
+import com.bacainc.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
